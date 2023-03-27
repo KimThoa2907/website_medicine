e2e630e3ffa287b6e8e412f5261c91de1bd73527
LAPTOP-MTGPP3N7\Admin <phanthikimthoa.ltp@gmail.com>
2023-03-27 12:42:12 +0700

    commit first

---
+++ admin/login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<head>
+<meta charset="utf-8">
+<title>Login</title>
+    <Link rel="stylessheet" type="text/css" href="css/styleslogin.css" media="screen"/> 
+</head>
+<body>
+<div class="container">
+    <section id= "content">
+        <form action="" method="post">
+            <h1> Admin login</h1>
+            <div>
+                <input type ="text" placeholder="Username" required="" name="AdminUser"/>
+            </div>
+            <div>
+                <input type="password" placeholder="Password" required="" name="AdminPassw"/>
+            </div>
+            <div>
+                <input type="submit" value="Log in"/>
+            </div>
+    
+        </form> <!--form-->
+        <div>
+            <a href="#"></a>
+        </div>
+    </section>
+
+</div>
+</body>
